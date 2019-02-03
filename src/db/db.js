6c4a1c139f42cd04e44f1357c473ecc9d605ed84
dalborgo <Montebaldo1>
2019-02-03 02:14:41 +0100

    casa

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,6 @@
 const couchbase = require('couchbase')
-const ottoman = require('ottoman')
-const cluster = new couchbase.Cluster(`couchbase://10.0.2.36`)
+const ottoman = require('ottoman-in')
+const cluster = new couchbase.Cluster(`couchbase://127.0.0.1`)
 const bucket = cluster.openBucket('ottoman', '902100')
 ottoman.store = new ottoman.CbStoreAdapter(bucket, couchbase)
 
--- src/models/chat.js
+++ src/models/chat.js
@@ -1,26 +1,25 @@
-import {getFunctions} from './helpers'
+import { getFunctions } from './helpers'
 import {
   ottoman
 } from '../db'
 import { hashSync } from 'bcryptjs'
 import User from './user'
-import util from "util"
+import util from 'util'
 
 const Chat = ottoman.model('Chat', {
   title: 'string',
-  users: [{ref: 'User'}],
-  lastMessage: {ref: 'Message'},
-  createdAt: {type: 'Date', default: Date.now},
+  users: [{ ref: 'User' }],
+  lastMessage: { ref: 'Message' },
+  createdAt: { type: 'Date', default: Date.now },
   updateAt: 'Date'
 })
 getFunctions.call(Chat)
 const USER_LIMIT = 5
 Chat.pre('save', async function (chat, next) {
-  if(!this.title){
-    const usersForTitle = this.users.slice(0,USER_LIMIT)
-    await User.getAll(usersForTitle)
-    let title = usersForTitle.map(u => u.name).join(', ')
-    if(this.users.length > USER_LIMIT){
+  if (!this.title) {
+    const users = await User.search({ username: { $in: this.users } }, { limit: USER_LIMIT })
+    let title = users.map(u => u.name).join(', ')
+    if (this.users.length > USER_LIMIT) {
       title += '...'
     }
     this.title = title
