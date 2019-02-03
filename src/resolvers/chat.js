e0d1725f0972b15a3acc9fc9173692e490ebccd5
dalborgo <Montebaldo1>
2019-02-03 22:23:29 +0100

    casa

--- src/resolvers/chat.js
+++ src/resolvers/chat.js
@@ -3,42 +3,42 @@ import { startChat } from '../schemas'
 import { UserInputError } from 'apollo-server-express'
 import { Chat, User, Message } from '../models'
 
-async function updateUsers(users, ref) {
-  const promises =users.map(u => {
+async function updateUsers (users, ref) {
+  const promises = users.map(u => {
     u.chats = u.chats ? [...u.chats, ref] : [ref]
     return u.commit()
   })
-  await Promise.all(promises);
+  await Promise.all(promises)
 }
 
 export default {
   Mutation: {
-    startChat: async (root, args, {req}, info) => {
-      let {title, userIds} = args
-      const {userId} = req.session
-      await Joi.validate(args, startChat(userId), {abortEarly: false})
+    startChat: async (root, args, { req }, info) => {
+      let { title, userIds } = args
+      const { userId } = req.session
+      await Joi.validate(args, startChat(userId), { abortEarly: false })
       const other_user = userIds.map(u => User.ref(u))
-      const users=other_user.concat([ User.ref(userId)])
+      const users = other_user.concat([User.ref(userId)])
       await User.getAll(users)
       const valid_users = other_user.filter(f => f.loaded())
-      if(valid_users.length !== userIds.length){
+      if (valid_users.length !== userIds.length) {
         throw new UserInputError('ID utente non trovato!')
       }
-      const chat = await Chat.createAndSave({title, users})
+      const chat = await Chat.createAndSave({ title, users })
       const chat_ref = Chat.ref(chat.id())
       await updateUsers(users, chat_ref)
       return chat
     }
   },
-  Chat : {
-    messages : async (chat, args, context, info) =>{
-      return Message.search({chat:chat.id()})
+  Chat: {
+    messages: async (chat, args, context, info) => {
+      return Message.search({ chat: Chat.ref(chat.id()) })
     },
-    users : async (chat, args, context, info) =>{
+    users: async (chat, args, context, info) => {
       await chat.expand('users')
       return chat.users
     },
-    lastMessage : async (chat, args, context, info) =>{
+    lastMessage: async (chat, args, context, info) => {
       await chat.expand('lastMessage')
       return chat.lastMessage
     }
